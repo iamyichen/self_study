5377b2177a3f70082ccdb70ce0e20f2b1cfa886f
Sylar Li <32724604+iamyichen@users.noreply.github.com>
2018-03-03 06:59:32 +0000

    Update 类属性继承方式的探索.py

--- 类属性继承方式的探索.py
+++ 类属性继承方式的探索.py
@@ -1,6 +1,7 @@
 '''
 实验表明，类属性在继承的过程中既不是深拷贝
-也不是浅拷贝，而是子类属性对父类属性的引用，
+也不是浅拷贝，而是通过字典的方式传递的，当
+访问累类的属性时，一次查找本类，父类。
 因此，改变父类的属性会影响到子类，而改变子
 类的属性不会影响到父类
 '''
@@ -14,7 +15,10 @@ class C(A):
     pass
 
 print(A.x,B.x,C.x)
+print(A.__dict__,B.__dict__,C.__dict__)
 B.x=2
 print(A.x,B.x,C.x)
+print(A.__dict__,B.__dict__,C.__dict__)
 A.x=3
 print(A.x,B.x,C.x)
+print(A.__dict__,B.__dict__,C.__dict__)
